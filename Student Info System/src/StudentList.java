31fa449a6b09ff71a2142e28b06313d0122f2f90
Zoe <Zoe@192.168.1.125>
2014-10-20 17:17:14 -0600

    started constructor

--- Student Info System/src/StudentList.java
+++ Student Info System/src/StudentList.java
@@ -1,5 +1,36 @@
+import java.util.ArrayList;
 
 public class StudentList
 	{
-
+	
+	private String lastName;
+	private String firstName;
+	private String courseOne;
+	private String gradeOne;
+	private String courseTwo;
+	private String gradeTwo;
+	private String courseThree;
+	private String gradeThree;
+	private double gpa;
+	
+	public StudentList(String l, String f, String co, String go, String ct, String gt, String cth, String gth, double g)
+	{
+		lastName = l;
+		firstName = f;
+		courseOne = co;
+		gradeOne = go;
+		courseTwo = ct;
+		gradeTwo = gt;
+		courseThree = cth;
+		gradeThree = gth;
+		gpa = g;
+	}
+	
+	
+		
+	// loop through data file and add to array list
+	// constructor needs all 10 parts
+	
+	
+	ArrayList <Student> StudentList = new ArrayList <Student>();
 	}
